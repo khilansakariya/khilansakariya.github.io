38c2b2511cdcfbd27fdf58758624bc488e4ede7c
khilansakariya <khilansakariya123@yahoo.com>
2025-10-29 16:47:40 +0530

    Fix robots.txt and sitemap.xml - Remove non-existent URLs and hash links, update deployment workflow

--- src/app/robots.ts
+++ src/app/robots.ts
@@ -2,11 +2,13 @@ import { MetadataRoute } from 'next'
 
 export default function robots(): MetadataRoute.Robots {
   return {
-    rules: {
-      userAgent: '*',
-      allow: '/',
-      disallow: ['/private/', '/admin/'],
-    },
+    rules: [
+      {
+        userAgent: '*',
+        allow: '/',
+        disallow: ['/private/', '/admin/', '/api/'],
+      },
+    ],
     sitemap: 'https://khilansakariya.github.io/sitemap.xml',
   }
 } 
